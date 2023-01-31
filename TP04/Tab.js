6dd4d79c13ab695ba29321a370b503d64b11c3e8
Tuon-Sreynit <tuonsreynit83@gmail.com>
2023-01-31 11:07:58 +0700

    tp2

---
+++ TP04/Tab.js
@@ -0,0 +1,7 @@
+var his = document.getElementById("history");
+var vis = document.getElementById("vision");
+var goal = document.getElementById("goal");
+function openContent(){
+    content.innerHTML = ``
+    
+}
